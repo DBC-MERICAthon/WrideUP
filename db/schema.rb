b956a099babd79ea01a41488633855b50e65cfea
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2015-07-01 16:43:16 -0700

    pull changes and change to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150701012538) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "drivers", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
